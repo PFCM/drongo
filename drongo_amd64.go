5b905fa17c8133f686e43ca66a21ab08ce525b3b
Paul Mathews <pfcmathews@gmail.com>
2026-06-21 00:19:40 +1200

    maybe fix amd64?

--- drongo_amd64.go
+++ drongo_amd64.go
@@ -3,10 +3,12 @@
 package drongo
 
 import (
+	"github.com/pfcm/drongo/arch/all"
 	"github.com/pfcm/drongo/arch/amd64"
-	"github.com/pfcm/drongo/arch/any"
 )
 
-var AddFloat32 = amd64.AddFloat32
-
-var AbsoluteFloat64 = any.AbsoluteFloat64
+var (
+	AddFloat32      = amd64.AddFloat32
+	AbsoluteFloat64 = all.AbsoluteFloat64
+	ClipFloat32     = all.ClipFloat32
+)
